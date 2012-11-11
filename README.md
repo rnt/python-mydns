1b8300608bafe83a03deab4c3f14f299ff77e4ac
Renato Covarrubias <rnt@rnt.cl>
2012-11-11 20:07:51 -0300

    Adding setup.py

--- README.md
+++ README.md
@@ -2,11 +2,14 @@ python-mydns
 ========
 
 Author: Renato Covarrubias
-
 License: GPL-3
 
 Based on Milan Nikolic works. This is only a library, not a script.
 
 ### Using
 
-See mydns.html for examples ;)
+  See mydns.html for examples ;)
+  
+### Packaging
+
+  In http://repo.rnt.cl you will find this RPM for Fedora 17.
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import sys
+
+# Python 3 conversion
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(
+    name='python-mydns',
+    version='1.0',
+    description="A library to connect to mydns mysql database and manage them.",
+    author='Renato Covarrubias',
+    author_email='rnt [at] rnt.cl]',
+    packages=['mydns'],
+    license='GPL-3.0',
+    url='https://github.com/rnt/python-mydns',
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GPL-3.0 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    **extra
+)
